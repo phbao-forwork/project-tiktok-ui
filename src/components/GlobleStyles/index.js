45ede06a88857f6acc2f631dee6fa825537aace2
hbao-newbie <baob1906432@student.ctu.edu.vn>
2023-01-15 13:28:03 +0700

    fix issue much files index.js

--- src/components/GlobleStyles/index.js
+++ src/components/GlobleStyles/index.js
@@ -1,12 +1,1 @@
-import PropTypes from 'prop-types';
-import './GlobleStyles.scss';
-
-function GlobleStyles({ children }) {
-    return children;
-}
-
-GlobleStyles.propTypes = {
-    children: PropTypes.node.isRequired,
-};
-
-export default GlobleStyles;
+export { default } from './GlobleStypes';
